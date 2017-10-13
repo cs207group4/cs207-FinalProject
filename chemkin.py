fd71a1d511d702f7e419cb82ee154d626a948812
Jeffery <zzy8200@gmail.com>
2017-10-13 13:01:22 -0400

    update chemkin.py

--- chemkin.py
+++ chemkin.py
@@ -2,10 +2,6 @@ import xml.etree.ElementTree as ET
 import numpy as np
 from copy import deepcopy
 
-import xml.etree.ElementTree as ET
-import numpy as np
-from copy import deepcopy
-
 class InputParser:
     
     def __init__(self, file_name):
