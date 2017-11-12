a5380263dc63d3dd76b3994d8f024e0cfbefc2d9
xuwd11 <31458899+xuwd11@users.noreply.github.com>
2017-11-12 01:24:40 -0500

    updated chemkin

--- make_chemkin.sh
+++ make_chemkin.sh
@@ -5,4 +5,10 @@ echo '' >> chemkin.py
 cat modules/ReactionCoeffs.py >> chemkin.py
 echo '' >> chemkin.py
 echo '' >> chemkin.py
+cat modules/SQLParser.py >> chemkin.py
+echo '' >> chemkin.py
+echo '' >> chemkin.py
+cat modules/BackwardCoeffs.py >> chemkin.py
+echo '' >> chemkin.py
+echo '' >> chemkin.py
 cat modules/chemkin.py >> chemkin.py
