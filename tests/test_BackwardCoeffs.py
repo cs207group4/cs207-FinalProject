414a9ce74879da6c7e1360deff301625b439f6e4
Weidong <xuwd11@gmail.com>
2017-11-15 18:36:42 -0500

    updated test

--- tests/test_BackwardCoeffs.py
+++ tests/test_BackwardCoeffs.py
@@ -1,4 +1,3 @@
-
 import sys
 import numpy as np
 from pychemkin import *
@@ -10,7 +9,10 @@ def test_CPcalc_single():
     Cp_r = bc.Cp_over_R(500.)
     
 def test_Hcalc_single():
-    pass
+    ip = InputParser('tests/test_xml/rxns_singlereversible.xml')
+    sql = SQLParser('pychemkin/data/thermo.sqlite')
+    bc = BackwardCoeffs(ip.nu_react, ip.nu_prod, ip.species, sql)
+    Cp_r = bc.H_over_RT(500.)
 
 def test_coeffs_single():
     ip = InputParser('tests/test_xml/rxns_singlereversible.xml')
@@ -23,5 +25,27 @@ def test_coeffs_single():
     kb = bc.backward_coeffs(kf, 500)
 
 
-def test_coeffs():
-    pass
+def test_coeffs_None():
+    ip = InputParser('tests/test_xml/rxns_singlereversible.xml')
+    sql = SQLParser('pychemkin/data/thermo.sqlite')
+    bc = BackwardCoeffs(ip.nu_react, ip.nu_prod, ip.species, sql)
+    bc.coeffs = None
+    bc.H_over_RT(500)
+    bc.coeffs = None
+    bc.S_over_R(500)
+    
+def test_no_reversible():
+    ip = InputParser('tests/test_xml/rxns_singlereversible.xml')
+    sql = SQLParser('pychemkin/data/thermo.sqlite')
+    bc = BackwardCoeffs(ip.nu_react, ip.nu_prod, ip.species, sql)
+    bc.backward_coeffs(np.array([]), 500)
+    
+def test_dimension():
+    ip = InputParser('tests/test_xml/rxns_singlereversible.xml')
+    sql = SQLParser('pychemkin/data/thermo.sqlite')
+    bc = BackwardCoeffs(ip.nu_react, ip.nu_prod, ip.species, sql)
+    try:
+        bc.backward_coeffs(np.array([1, 2, 3]), 500)
+    except ValueError as e:
+        assert type(e) == ValueError
+        print(e)
