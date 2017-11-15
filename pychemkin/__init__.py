9b133ad93859611d61b44589da9ebc28fc9da211
Weidong <xuwd11@gmail.com>
2017-11-15 18:22:58 -0500

    fix __init__.py

--- pychemkin/__init__.py
+++ pychemkin/__init__.py
@@ -1,3 +1,12 @@
+import numpy as np
+import xml.etree.ElementTree as ET
+from copy import deepcopy
+import re
+from bs4 import BeautifulSoup
+import sqlite3
+import pandas as pd
+import os
+
 from .InputParser import InputParser
 from .SQLParser import SQLParser
 from .BackwardCoeffs import BackwardCoeffs
