bf787482ed85c4fe7611e1a98e6ecdb8b724b55a
Weidong <xuwd11@gmail.com>
2017-11-15 17:59:12 -0500

    deleted SQLBuilder

--- pychemkin/__init__.py
+++ pychemkin/__init__.py
@@ -9,7 +9,6 @@ import os
 
 from .InputParser import InputParser
 from .SQLParser import SQLParser
-from .SQLBuilder import SQLBuilder
 from .BackwardCoeffs import BackwardCoeffs
 from .ReactionCoeffs import ReactionCoeffs
 from .chemkin import chemkin
