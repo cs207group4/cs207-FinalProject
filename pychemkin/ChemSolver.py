f2a2cbbfaf314b28a1df95cfbaee30dfe2b9ff72
j6626 <janehuang212@gmail.com>
2017-11-27 11:06:17 -0500

    testing out equilibrium solutions

--- pychemkin/ChemSolver.py
+++ pychemkin/ChemSolver.py
@@ -6,25 +6,25 @@ from .chemkin import chemkin
 
 class ChemSolver:
     '''
-    The ChemSolver module wrap around SciPy's ODE solver to obtain the evolution of concentrations and reaction 
+    The ChemSolver module wrap around SciPy's ODE solver to obtain the evolution of concentrations and reaction
     rates over time
-    
+
     METHODS and ATTRIBUTES
     ========
     After initialization, user could call:
-     - solve(y0, T, t_span, t_eval=None, **options): method to solve the differential equations given an initial 
+     - solve(y0, T, t_span, t_eval=None, **options): method to solve the differential equations given an initial
        value of species concentrations:
            dy / dt = chemkin.reaction_rate(y, T)
            y(t0) = y0
-     - get_results(return_reaction_rate=True): method to return the solution of ODEs, which includes an array of 
-       time points, an array of solution values (species concentrations) at each time point, and an array of 
+     - get_results(return_reaction_rate=True): method to return the solution of ODEs, which includes an array of
+       time points, an array of solution values (species concentrations) at each time point, and an array of
        reaction rates (optional)
      - save_results(file_name): method to save the solution of ODEs to a csv or hdf5 file.
      - load_results(file_name): method to load the solution of ODEs from a csv or hdf5 file storing the data
-     - grid_search(y0s, Ts, t_span, return_reaction_rate=True, **options): method to solve ODE at different 
+     - grid_search(y0s, Ts, t_span, return_reaction_rate=True, **options): method to solve ODE at different
        combinations of starting concentrations and temperatures
      - get_grid_search_result(): method to return the grid search starting conditions and results
-     
+
     EXAMPLES
     =========
     >>> chem = chemkin("tests/test_xml/rxns.xml")
@@ -51,14 +51,47 @@ class ChemSolver:
         self.T = None
         self.grid_condition = None
         self.grid_result = None
-        
+
     def __dy_dt(self, t, y):
         r = self.chem._progress_rate_default_T(y.reshape(-1,1),self.kf,self.kb)
         return np.sum(r * self.nu_diff_matrix, axis=1)
-    
-    def solve(self, y0, T, t1, dt, method='lsoda', **options):
+    """
+    Needs further testing
+    def _jac(self, t, y):
+        '''
+        Return Jacobian of right-hand side of concentration ODEs
+
+        INPUT
+        =====
+        '''
+        if self.T is None:
+            raise ValueError("T needs to be set")
+        N, M = self.chem.nu_react.shape
+        derivs = np.zeros((N,M))
+        y = np.array(y).reshape(-1,1)
+        #calculate partial derivatives of progress rates with respect to the species concentrations
+        for i in range(N):
+            prod1 = y**self.chem.nu_react
+            masked_prod1 = np.ma.array(prod1, mask = False)
+            masked_prod1[i] = True
+
+            derivs[i] =self.kf*self.chem.nu_react[i]*y[i]**(self.chem.nu_react[i]-1)*np.product(masked_prod1, axis =0)
+            if np.any(self.chem.reversible):
+                prod2 = y**self.chem.nu_prod
+                masked_prod2 = np.ma.array(prod2, mask = False)
+                masked_prod2[i] = True
+                derivs[i,self.chem.reversible] = derivs[i,self.chem.reversible] -self.chem.nu_prod[i]*y[i]**(self.chem.nu_prod[i]-1)*  self.kb * np.product(masked_prod2[:, self.chem.reversible], axis=0)
+        nu_diff_matrix = self.chem.nu_prod-self.chem.nu_react
+        jac = np.zeros((N,N))
+        for j in range(N):
+            jac[:,j] = np.sum(derivs[j] * nu_diff_matrix, axis=1)
+
+        return jac
+    """
+
+    def solve(self, y0, T, t1, dt, algorithm='lsoda', use_jac = False, **options):
         '''Solve ODEs
-        
+
         INPUTS
         ======
         y0: array_like, shape(n,), required
@@ -69,8 +102,10 @@ class ChemSolver:
             Integration end point. The solver starts with t=0 and integrates until it reaches t=t1.
         dt: float, required
             Integration step.
-        method: string, optional
+        algorithm: string, optional
             Integration algorithm. default value is 'lsoda'
+        use_jac: boolean, optional
+            Option to use analytic Jacobian. Default value is False, where SciPy calculates the Jacobian using finite differences
         **options:
             Options passed to scipy.integrate.ode.set_integrator
         '''
@@ -78,7 +113,10 @@ class ChemSolver:
         # get variables not changed in ODE
         _,self.kf, self.kb = self.chem._progress_rate_init(y0, T)
         self.nu_diff_matrix = self.chem.nu_prod-self.chem.nu_react
-        r = ode(self.__dy_dt).set_integrator(method, **options)
+        if use_jac:
+            r = ode(self.__dy_dt, self._jac).set_integrator(algorithm, **options)
+        else:
+            r = ode(self.__dy_dt).set_integrator(algorithm, **options)
         r.set_initial_value(y0, 0)
         self._t = [0]
         self._y = [y0]
@@ -90,15 +128,15 @@ class ChemSolver:
         self._sol = True
         self.reaction_rate = None
         return self
-    
+
     def get_results(self, return_reaction_rate=True):
         '''Return ODE solutions
-        
+
         INPUT
         =====
         return_reaction_rate: bool, optional
             If True (default), return reaction rates
-        
+
         RETURNS
         =======
         t: ndarray, shape(n_points,)
@@ -119,14 +157,14 @@ class ChemSolver:
             return t, y, self.reaction_rate
         else:
             return t, y, None
-    
+
     def to_df(self):
         '''Save the solution of ODEs to a pandas dataframe
-        
+
         RETURNS
         =======
         df: pandas data frame containing the solution
-        
+
         '''
         if self._sol is None:
             raise ValueError('ODE not solved.')
@@ -141,10 +179,10 @@ class ChemSolver:
         values = np.concatenate((t.reshape(1, len(t)), y.reshape(len(y), len(t)), \
                                  rr.reshape(len(rr), len(t)), self.T * np.ones((1, (len(t))))), axis=0)
         return pd.DataFrame.from_records(values, cols).transpose()
-    
+
     def save_results(self, file_name):
         '''Save the solution of ODEs to a csv or hdf5 file
-        
+
         INPUT
         ======
         file_name: string, required
@@ -159,11 +197,11 @@ class ChemSolver:
             df.to_csv(file_name, index=False)
         else:
             df.to_hdf(file_name, 'df',index=False)
-        return self    
-    
+        return self
+
     def load_results(self, file_name):
         '''Load the solution of ODEs from a csv or hdf5 file
-        
+
         INPUT
         ======
         file_name: string, required
@@ -183,10 +221,10 @@ class ChemSolver:
         self.reaction_rate = df.iloc[:, 1+len(self.chem.species):1+2*len(self.chem.species)].values.transpose()
         self.T = df['T'][0]
         return self
-    
+
     def grid_search(self, y0s, Ts, t1, dt, method='lsoda', return_reaction_rate=True, **options):
         '''Solve ODEs at user specified combinations of starting concentrations and temperatures
-        
+
         INPUTS
         =======
         y0s: array_like, shape(n_y_cond, n), required
@@ -206,11 +244,11 @@ class ChemSolver:
         '''
         self.grid_result = {T:[self.solve(y0, T, t1, dt, method, **options).\
                                get_results(return_reaction_rate) for y0 in y0s] for T in Ts}
-        self.grid_condition = [Ts, y0s] 
+        self.grid_condition = [Ts, y0s]
         self._sol = None
         self.reaction_rate = None
         return self
-    
+
     def get_grid_results(self):
         '''Return the grid search starting conditions and results
         '''
