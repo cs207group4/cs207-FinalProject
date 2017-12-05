b3f0daf5850e018d968bde10f67089622fcdbb9f
j6626 <janehuang212@gmail.com>
2017-12-05 02:25:21 -0500

    removed extra lines from demo notebook

--- tests/test_ChemSolver.py
+++ tests/test_ChemSolver.py
@@ -7,7 +7,7 @@ def test_solve():
     T = 300
     t1 = 0.003
     dt = 5e-4
-    cs = ChemSolver(chem).solve(y0, T, t1, dt, method='lsoda')
+    cs = ChemSolver(chem).solve(y0, T, t1, dt, algorithm='lsoda')
     t, y, rr = cs.get_results(return_reaction_rate=False)
     assert rr is None
     t, y, rr = cs.get_results()
@@ -19,9 +19,9 @@ def test_grid_search():
     dt = 5e-4
     y0s = [np.ones(len(chem.species))*i for i in range(1, 4)]
     Ts = [300, 400]
-    gs = ChemSolver(chem).grid_solve(y0s, Ts, t1, dt, method='lsoda')
+    gs = ChemSolver(chem).grid_solve(y0s, Ts, t1, dt, algorithm='lsoda')
     _y1 = gs.get_grid_results()[1][300][0][1]
-    _y2 = ChemSolver(chem).solve(y0s[0], Ts[0], t1, dt, method='lsoda').get_results()[1]
+    _y2 = ChemSolver(chem).solve(y0s[0], Ts[0], t1, dt, algorithm='lsoda').get_results()[1]
     assert str(_y1) == str(_y2)
     print(_y1)
 
@@ -45,7 +45,7 @@ def test_IO():
     T = 300
     t1 = 0.003
     dt = 5e-4
-    cs = ChemSolver(chem).solve(y0, T, t1, dt, method='lsoda')
+    cs = ChemSolver(chem).solve(y0, T, t1, dt, algorithm='lsoda')
     cs.save_results('tests/test_data/test.csv')
     cs.save_results('tests/test_data/test.h5')
     cs1 = ChemSolver(chem).load_results('tests/test_data/test.csv')
