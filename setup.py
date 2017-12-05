c20d1757fa11beef420f19ac60e801e4d2677912
Jeffery <zzy8200@gmail.com>
2017-12-05 14:43:18 -0500

    new pypi

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='pychemkin',
 
-    version='1.1.0',
+    version='1.1.1',
 
     description='Chemical Kinetics Library',
     long_description=long_description,
